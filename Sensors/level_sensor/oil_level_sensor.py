c5989b3c73eaedb7fc372276c08a0e8ca0026c28
Roy <roymhelms@gmail.com>
2018-04-02 12:31:47 -0400

    All changes listed allow for the level and temperature sensors to communicate with the LCD display

--- Sensors/level_sensor/oil_level_sensor.py
+++ Sensors/level_sensor/oil_level_sensor.py
@@ -1,21 +1,37 @@
 from time import sleep
 import RPi.GPIO as GPIO
+import I2C_LCD_driver
 
 led = 22
 float = 18
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(float, GPIO.IN)
-GPIO.setup(led,GPIO.OUT)
+#GPIO.setup(led,GPIO.OUT)
 
-while True:
-	if GPIO.input(float) == 1:
-		GPIO.output(led, False)
-		#outMsg = "Oil level is HIGH"
-		sleep(.1)
-	if GPIO.input(float) == 0:
-		GPIO.output(led, True)
-		#outMsg = "Oil level is LOW"
-#print outMsg
+display = I2C_LCD_driver.lcd()
 
-GPIO.cleanup()
+def writedisp():
+    sleep (1)
+    outMsg = "No message yet"
+    if GPIO.input(float) == 1:
+	    #GPIO.output(led, False)
+	    outMsg = "Oil level=LOW  "
+	    sleep(.1)
+    if GPIO.input(float) == 0:
+	    #GPIO.output(led, True)
+            outMsg = "Oil level=HIGH"
+    print outMsg
+    display.lcd_display_string(outMsg)
+
+try:
+    while True:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(float, GPIO.IN)
+        writedisp()
+        GPIO.cleanup()
+
+except KeyboardInterrupt:
+    print "Cleaning"
+    display.lcd_display_string("              ")
+    GPIO.cleanup()
