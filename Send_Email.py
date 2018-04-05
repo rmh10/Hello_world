51169ed6d3fa40c6ff3218938e59eb7b761705ff
Roy Helms <roymhelms@gmail.com>
2018-04-05 14:31:40 -0400

    Remove Password

--- Send_Email.py
+++ Send_Email.py
@@ -5,4 +5,10 @@ from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
 
+<<<<<<< HEAD
+=======
+fromadd = [From Email]
+toadd = [To Email]
+password = [Instert Password]
+>>>>>>> 2d605c2... Remove Password
 
