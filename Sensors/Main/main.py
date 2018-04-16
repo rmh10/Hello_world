5dc2c860cdbcea93eb1c8ca6bc6112367f06f9e6
Roy Helms <roymhelms@gmail.com>
2018-04-16 16:11:53 -0400

    Made changes to make a main.py file

--- Sensors/Main/main.py
+++ Sensors/Main/main.py
@@ -20,22 +20,50 @@ import time
 #import smbus
 import smtplib
 import email
-#import I2C_LCD_driver
-#import temperature_sensor_code
-#import oil_level_sensor
+import I2C_LCD_driver
+import temperature_sensor_code
+import oil_level_sensor
+from time import sleep
+
+display = I2C_LCD_driver.lcd()
 
 #from email.MIMEMultipart import MIMEMulitpart
 #from email.MIMEText import MIMEText
 
-print (time.strftime("Time is: %H:%M:%S"))
+#print (time.strftime("Time is: %H:%M:%S"))
 
 # 1. Function to take readings from each sensor
-
-
+def read_sensors():
+	while true:
+		print (time.strftime("Time is: %H:%M:%S"))
+		sleep(2.5)
+		read_level()
+		sleep(2.5)
+		read_temp()
+		sleep(2.5)
+		print("Starting next reading.")
+	
 # 2. Function to output to the LCD
 
 
 # 3. Function to store readings into a file
-def buildFile(level, temperature, pressure)
-	if level = '0':
-		
+#def buildFile(level, temperature, pressure)
+
+
+try:
+    while True:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(float, GPIO.IN)
+        read_sensors()
+	GPIO.cleanup()
+
+except KeyboardInterrupt:
+    print "Cleaning"
+    display.lcd_display_string("              ")
+    GPIO.cleanup()try:
+    while True:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(float, GPIO.IN)
+        read_level()
+        GPIO.cleanup()
+
---
+++ Sensors/Main/oil_level_sensor.py
@@ -0,0 +1,37 @@
+from time import sleep
+import RPi.GPIO as GPIO
+import I2C_LCD_driver
+
+led = 22
+float = 18
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(float, GPIO.IN)
+#GPIO.setup(led,GPIO.OUT)
+
+display = I2C_LCD_driver.lcd()
+
+def read_level():
+    sleep (1)
+    outMsg = "No message yet"
+    if GPIO.input(float) == 1:
+	    #GPIO.output(led, False)
+	    outMsg = "Oil level=LOW  "
+	    sleep(.1)
+    if GPIO.input(float) == 0:
+	    #GPIO.output(led, True)
+            outMsg = "Oil level=HIGH"
+    print outMsg
+    display.lcd_display_string(outMsg)
+
+try:
+    while True:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(float, GPIO.IN)
+        read_level()
+        GPIO.cleanup()
+
+except KeyboardInterrupt:
+    print "Cleaning"
+    display.lcd_display_string("              ")
+    GPIO.cleanup()
