093581af50ccbd01d2038936094cfb65c74fa110
Roy <roymhelms@gmail.com>
2018-04-20 12:17:34 -0400

    Made changes to start ADC_program.py

--- Sensors/pressure_sensor/ADC_program.py
+++ Sensors/pressure_sensor/ADC_program.py
@@ -1,15 +1,20 @@
 from time import sleep
 import rpi.gpio as gpio
 import i2c_lcd_driver
+import spidev
 
-CS   = 22
-MOSI = 19
-MISO = 21
-SCLK = 23
+#CE0  = 24
+#MOSI = 19
+#MISO = 21
+#SCLK = 23
 
-RPI.GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(CS, GPIO.OUT)
+#RPI.GPIO
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(CS, GPIO.OUT)
+
+spi = spidev.SpiDev()
+spi.open(0,1)
 
 def init_ADC():
-	 
+    GPIO.out(CS, True)
+
