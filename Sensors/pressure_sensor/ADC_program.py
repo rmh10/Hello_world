2e7ceb6682a95ba29b2ed921863699590b1e8393
Roy <roymhelms@gmail.com>
2018-04-22 09:13:42 -0400

    Made changes to ADC_program.py to communicate with the ADC

--- Sensors/pressure_sensor/ADC_program.py
+++ Sensors/pressure_sensor/ADC_program.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python
+# -*- coding: Latin-1 -*-
+import os
+import sys
 from time import sleep
-import rpi.gpio as gpio
-import i2c_lcd_driver
+#import rpi.gpio as gpio
+#import i2c_lcd_driver
 import spidev
+import binascii
 
 #CE0  = 24
 #MOSI = 19
@@ -13,8 +18,16 @@ import spidev
 #GPIO.setup(CS, GPIO.OUT)
 
 spi = spidev.SpiDev()
-spi.open(0,1)
+spi.open(0,0)
+spi.mode=1
 
-def init_ADC():
-    GPIO.out(CS, True)
-
+try:
+     while True:
+         resp = spi.xfer([0x6000, 0x0000, 0x0000]) # transfer one byte
+         print('Recieved: 0x{0}'.format(binascii.hexlify(bytearray(resp))))
+         sleep(1) # sleep for 0.1 seconds
+            #end while
+#except KeyboardInterrupt: # Ctrl+C pressed, so…
+finally:
+    spi.close() # … close the port before exit
+     #end try
